2b902dce30eab9b1af69ff0f69f218a56a352e77
jsonks <jrobb@sekls.org>
2024-10-04 14:41:39 -0500

    Update IntranetUserJS.js

--- Project2: Print overdues/IntranetUserJS.js
+++ Project2: Print overdues/IntranetUserJS.js
@@ -1,3 +1,7 @@
+ if ($('body').is('#circ_circulation-home')) {
+//  var printOdueTesters = ['IOLA', 'PARSONS'];
+//  if ($.inArray(libcode, printOdueTesters) !== -1) {   
+  
   //Add link to print notices
   var today = new Date();
   var date = today.toISOString().split('T')[0];
@@ -11,7 +15,7 @@
   var pnDate2 = date;
   var dayOfWeek = today.getDay(); //Sunday = 0; Saturday = 6
   var pnLink = '/cgi-bin/koha/reports/guided_reports.pl?reports=' + pnReportNumber +'&phase=Run%20this%20report&template=' + pnTemplateID + '&sql_params=' + pnBranch +'&sql_params=' + pnDate1 +'&sql_params=' + pnDate2 +'&param_name=Choose%20Library%7Cbranches&param_name=Notices%20for%7Cdate&param_name=and%7Cdate';
-    var pnLink2 = '/cgi-bin/koha/reports/guided_reports.pl?reports=' + pnReportNumber +'&phase=Run%20this%20report&template=' + pnTemplateID + '&sql_params=' + pnBranch +'&sql_params=' + pnDate2 +'&sql_params=' + pnDate2 +'&param_name=Choose%20Library%7Cbranches&param_name=Notices%20for%7Cdate&param_name=and%7Cdate';
+  var pnLink2 = '/cgi-bin/koha/reports/guided_reports.pl?reports=' + pnReportNumber +'&phase=Run%20this%20report&template=' + pnTemplateID + '&sql_params=' + pnBranch +'&sql_params=' + pnDate2 +'&sql_params=' + pnDate2 +'&param_name=Choose%20Library%7Cbranches&param_name=Notices%20for%7Cdate&param_name=and%7Cdate';
   
  if (dayOfWeek == 1){ //Monday
   $("#circ_circulation-home h3:contains('Overdues')").next().append('<fieldset class="brief" id="oduePrintDate"><span style="font-size: 110%; font-weight: bold;"><i class="fa fa-print"></i> Print notices for: </span><div><a class="btn btn-primary btn-sm" style="width: 100%; font-weight: bold; margin-top: 10px;" href="'+ pnLink + '" title="Includes Sunday and Monday">Today</a></div><hr><div><span >Or choose a specific date:</span><input id="pnFlatpickr" size="12"> <a type="submit" id="pnDatePickGo" class="btn btn-primary btn-sm" style="margin-bottom: 4px; font-weight: bold;" href="'+ pnLink + '">Go</a></div></fieldset>');
@@ -34,7 +38,7 @@
     $('#pnDatePickGo').attr('href', pnLink3);
   });
 
-  }
+//  }
  }
 
 
